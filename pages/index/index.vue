6c7e4ef7ccce22c34f54c7a9f146aeb726e9ad3a
wuzhe <wuzhe_@aliyun.com>
2020-09-26 23:23:44 +0800

    新增分享给朋友和朋友圈

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -24,39 +24,39 @@
 						</view>
 					</swiper-item>
 					<swiper-item>
-						<view class="swiper-item uni-bg-green">
+						<view class="swiper-item uni-bg-green base-info">
 							<scroll-view scroll-y="true" class="scroll-Y" :style="{height: windowHeight + 'px'}">
 								<view class="area">
-									<view class="theme">
-										<text>1</text>基本资料
+									<view class="theme d-flex horizontal v-center">
+										<text class="number">1</text>基本资料
 									</view>
-									<view class="row">
-										<view class="left">
+									<view class="row d-flex dashed-border horizontal justify fill">
+										<view class="left fill">
 											<text class="label">姓名：</text>
 											<text class="item">{{ resume.name }}</text>
 										</view>
-										<view class="right">
+										<view class="right fill">
 											<text class="label">电话：</text>
 											<text class="item">{{ resume.tel }}</text>
 										</view>
 									</view>
-									<view class="row">
-										<view class="left">
+									<view class="row d-flex dashed-border horizontal justify">
+										<view class="left fill">
 											<text class="label">性别：</text>
 											<text class="item">{{ resume.sex }}</text>
 										</view>
-										<view class="right">
+										<view class="right fill">
 											<text class="label">年龄：</text>
 											<text class="item">{{ resume.age }}</text>
 										</view>
 									</view>
-									<view class="row">
+									<view class="row dashed-border">
 										<view class="left">
 											<text class="label">邮箱：</text>
 											<text class="item">{{ resume.email }}</text>
 										</view>
 									</view>
-									<view class="row">
+									<view class="row dashed-border">
 										<view class="left">
 											<text class="label">住址：</text>
 											<text class="item">{{ resume.address }}</text>
@@ -64,8 +64,8 @@
 									</view>
 								</view>
 								<view class="area">
-									<view class="theme">
-										<text>2</text>主要技能
+									<view class="theme d-flex horizontal v-center">
+										<text class="number">2</text>主要技能
 									</view>
 									<view class="uni-padding-wrap uni-common-mt">
 										<view class="progress-box" v-for="(skill, index) in resume.skill" :key="index">
@@ -75,15 +75,15 @@
 									</view>
 								</view>
 								<view class="area">
-									<view class="theme d-flex v-center h-center">
-										<text>3</text>教育经历
+									<view class="theme d-flex horizontal v-center">
+										<text class="number">3</text>教育经历
 									</view>
-									<view class="edu-wrapper" v-for="(edu, index) in resume.education" :key="index">
-										<view class="d-flex horizontal v-center">
+									<view class="edu-wrapper" :class="{'dashed-border': index < resume.education.length - 1}" v-for="(edu, index) in resume.education" :key="index">
+										<view class="school">
 											<image class="school-img" src="../../static/icon_school.png" mode="widthFix"></image>
 											{{ edu.school }}
 										</view>
-										<view class="d-flex horizontal jusify v-center">
+										<view class="content d-flex horizontal justify v-center">
 											<view class="left">
 												<image class="comm-img" src="../../static/icon_book.png" mode="widthFix"></image>
 												{{ edu.major }}
@@ -93,22 +93,85 @@
 												{{ edu.year }}
 											</view>
 										</view>
-										<view>
+										<rich-text class="desc">
 											{{ edu.desc }}
+										</rich-text>
+									</view>
+								</view>
+								<view class="page">1/4</view>
+							</scroll-view>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="swiper-item uni-bg-blue base-info">
+							<scroll-view scroll-y="true" class="scroll-Y" :style="{height: windowHeight + 'px'}">
+								<view class="area">
+									<view class="theme d-flex horizontal v-center">
+										<text class="number">4</text>工作经历
+									</view>
+									<view class="edu-wrapper" :class="{'dashed-border': index < resume.work.length - 1}" v-for="(work, index) in resume.work" :key="index">
+										<view class="school">
+											<image class="school-img" src="../../static/icon_company.png" mode="widthFix"></image>
+											{{ work.company }}
+										</view>
+										<view class="content d-flex horizontal justify v-center">
+											<view class="left">
+												<image class="comm-img" src="../../static/icon_job.png" mode="widthFix"></image>
+												{{ work.job }}
+											</view>
+											<view class="right">
+												<image class="comm-img" src="../../static/icon_calendar.png" mode="widthFix"></image>
+												{{ work.year }}
+											</view>
+										</view>
+										<rich-text class="desc">
+											{{ work.desc }}
+										</rich-text>
+									</view>
+								</view>
+								<view class="page">2/4</view>
+							</scroll-view>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="swiper-item uni-bg-blue base-info">
+							<scroll-view scroll-y="true" class="scroll-Y" :style="{height: windowHeight + 'px'}">
+								<view class="area">
+									<view class="theme d-flex horizontal v-center">
+										<text class="number">5</text>项目经历
+									</view>
+									<view class="edu-wrapper" :class="{'dashed-border': index < resume.project.length - 1}" v-for="(project, index) in resume.project" :key="index">
+										<view class="school">
+											<image class="school-img" src="../../static/icon_computer.png" mode="widthFix"></image>
+											{{ project.name }}
+										</view>
+										<view>
+											<rich-text class="desc">
+												{{ project.desc }}
+											</rich-text>
 										</view>
 									</view>
 								</view>
-								</scroll-view>
+								<view class="page">3/4</view>
+							</scroll-view>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="swiper-item uni-bg-blue">
+							<view class="swiper-item uni-bg-blue base-info">
+								<view class="area">
+									<view class="theme d-flex horizontal v-center">
+										<text class="number">6</text>自我评价
+									</view>
+									<view class="edu-wrapper">
+										<rich-text class="desc">
+											{{ resume.introduction }}
+										</rich-text>
+									</view>
+								</view>
 							</view>
-					</swiper-item>
-					<swiper-item>
-						<view class="swiper-item uni-bg-blue">工作经历</view>
-					</swiper-item>
-					<swiper-item>
-						<view class="swiper-item uni-bg-blue">项目经历</view>
-					</swiper-item>
-					<swiper-item>
-						<view class="swiper-item uni-bg-blue">自我评价</view>
+						</view>
+						<view class="page">4/4</view>
 					</swiper-item>
 				</swiper>
 			</view>
@@ -130,7 +193,7 @@
 				  sex: "男",
 				  age: 27,
 				  email: "wuzhe_@aliyun.com",
-				  address: "深圳市福田区上沙街道龙秋路",
+				  address: "深圳市福田区上沙街道龙秋路5巷24号",
 				  skill:[
 				    { name: "Java", percent:85},
 				    { name: "PHP", percent: 40 },
@@ -193,6 +256,19 @@
 		onLoad() {
 
 		},
+		// 分享给朋友
+		onShareAppMessage () {
+			return {
+				title: '吴哲的简历',
+				path: '/pages/index/index'
+			}
+		},
+		// 分享到朋友圈
+		onShareTimeline () {
+			return {
+				title: '吴哲的简历'
+			}
+		},
 		methods: {
 
 		}
@@ -201,6 +277,8 @@
 <style>
 	page {
 		height: 100%;
+		word-wrap: break-word;
+		word-break: break-all;
 	}
 </style>
 <style lang="scss" scoped>
@@ -301,5 +379,65 @@
 }
 .comm-img {
 	width: 16px;
+	margin-right: 3px;
+}
+
+/* 基本资料 */
+.dashed-border {
+	border-bottom: dashed 1px #333;
+}
+.base-info {
+	padding: 0 20px;
+	.area {
+		margin-bottom: 30px;
+	}
+	.row {
+		padding: 10px 0;
+		.fill {
+			text-align: left;
+		}
+	}
+}
+.theme {
+	margin: 10px 0;
+	padding-bottom: 5px; 
+	border-bottom: 1px solid #333;
+	font-size: 20px;
+	.number {
+		display: block;
+		width: 22px;
+		height: 22px;
+		font-size: 16px;
+		line-height: 22px;
+		text-align: center;
+		font-weight: bold;
+		background-color: #444;
+		color: #fff;
+		margin-right: 10px;
+	}
+}
+.edu-wrapper {
+	padding: 10px 0;
+	.school {
+		margin-bottom: 10px;
+		.school-img {
+			vertical-align: -6px;
+			margin-right: 3px;
+			
+		}
+	}
+	.content {
+		margin-bottom: 10px;
+	}
+	.desc {
+		color: #999;
+		line-height: 1.5;
+	}
+}
+.page {
+	width: 100%;
+	font-size: 16px;
+	text-align: center;
+	padding-bottom: 15px;
 }
 </style>
